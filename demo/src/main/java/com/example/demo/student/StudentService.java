c0d7b52772bb71ac13b74e4538837bbfbbb66265
manos-fr <manoskakarakis@gmail.com>
2021-09-21 17:16:17 +0300

    implements put request with proper checks and exceptions

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -3,9 +3,11 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -27,15 +29,39 @@ public class StudentService {
     public void addNewStudent(Student student) {
         Optional<Student> studentOptional = studentRepository
                 .findStudentByEmail((student.getEmail()));
-        if (studentOptional.isPresent()){
+        if (studentOptional.isPresent()) {
             throw new IllegalStateException(("email taken"));
         }
         studentRepository.save(student);
     }
 
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository
+                .findById((studentId))
+                .orElseThrow(() -> new IllegalStateException(
+                        "student with id" + studentId + "does not exist"
+                ));
+        if (name != null &&
+                name.length() > 0 &&
+                !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+        if (email != null &&
+                email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)){
+            Optional<Student> studentOptional = studentRepository
+                    .findStudentByEmail(email);
+            if (studentOptional.isPresent()){
+                throw new IllegalStateException(("email taken"));
+            }
+            student.setEmail(email);
+        }
+    }
+
     public void deleteStudent(Long studentId) {
         boolean exists = studentRepository.existsById(studentId);
-        if (!exists){
+        if (!exists) {
             throw new IllegalStateException(
                     "student with id" + studentId + " does not exists");
         }
