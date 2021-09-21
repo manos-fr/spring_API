e63a688ef948044c2af86badec1578588a7267a8
manos-fr <manoskakarakis@gmail.com>
2021-09-21 13:15:58 +0300

    implements delete request with exception and checks if exists

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -28,6 +28,12 @@ public class StudentController {
 
         studentService.addNewStudent(student);
     }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(
+            @PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
     }
 
 
